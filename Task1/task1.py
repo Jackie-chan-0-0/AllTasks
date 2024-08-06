7b77b9b61f1bc57b7b64b50999428da5079eea6e
Boris Tsvetkov <GetNicePlay@gmail.com>
2024-08-06 14:36:31 +0300

    Commit v2.0

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,9 +1,35 @@
-input_user_n = int(input("Введите значение n: "))
-input_user_m = int(input("Введите значение m: "))
-i = 1
-print("Полученный путь: ", end='')
-while True:
-    print(i, end='')
-    i = 1 + (i + input_user_m - 2) % input_user_n
-    if i == 1:
-        break
+class PathCalculator:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.path = []
+
+    def calculate_path(self):
+        """Вычисляет путь по кругу."""
+        current_position = 1
+        while True:
+            self.path.append(current_position)
+            current_position = 1 + (current_position + self.m - 2) % self.n
+            if current_position == 1:
+                break
+
+    def display_path(self):
+        """Выводит полученный путь."""
+        print("Полученный путь:", ''.join(map(str, self.path)))
+
+
+def main():
+    # Получение входных данных от пользователя
+    input_user_n = int(input("Введите значение n: "))
+    input_user_m = int(input("Введите значение m: "))
+
+    # Создание экземпляра класса
+    path_calculator = PathCalculator(input_user_n, input_user_m)
+
+    # Вычисление и вывод пути
+    path_calculator.calculate_path()
+    path_calculator.display_path()
+
+
+if __name__ == "__main__":
+    main()
